fa50b1395d8122bf1813ad08993a348c47f1d772
cjoy3710 <cathycathyjoyjoy@gmail.com>
2020-09-28 23:26:01 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,36 @@ import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() {
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let pronoun = ["A", "My"];
+  let subject = ["computer", "car", "truck", "bakery", "magician"];
+  let action = ["melted", "got stranded", "broke down", "froze", "flew away"];
+  let where = [
+    "on the highway.",
+    "at the football stadium.",
+    "in a forrest.",
+    "on my roof."
+  ];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[pronounIndex] +
+    " " +
+    subject[subjectIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    where[whereIndex]
+  );
 };
